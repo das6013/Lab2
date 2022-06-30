a6c30fbf3832b6351efb0a156ac8821228a0a148
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 12:34:30 +0700

    Update observeobject.cpp

--- observeobject.cpp
+++ observeobject.cpp
@@ -4,34 +4,35 @@
 
 void ObserveObject::Attach(FileObserver *observer)
 {
-    subscribers.push_back(observer);
+    subscribers.push_back(observer);//add subcsribres in observe
 }
 
 void ObserveObject::Detach(FileObserver *observer)
 {
-    subscribers.erase(std::remove(subscribers.begin(), subscribers.end(), observer), subscribers.end());
+    subscribers.erase(std::remove(subscribers.begin(), subscribers.end(), observer), subscribers.end());//delete subcsribres in observe
 }
 
-void ObserveObject::Notify(bool existence, long size){
-    for(std::vector<FileObserver*>::const_iterator iter = subscribers.begin(); iter != subscribers.end(); iter++)
+void ObserveObject::Notify(bool existence, long size)//update data 
+{
+    for(std::vector<FileObserver*>::const_iterator iter = subscribers.begin(); iter != subscribers.end(); iter++)//iter to sub 
     {
         if (*iter != 0)
         {
-            (*iter)->Update(existence, size);
+            (*iter)->Update(existence, size);//update data
         }
     }
 }
 
 FileMonitor::FileMonitor(QString name) : prevFileState(name) { }
 
-void FileMonitor::CheckFile()
+void FileMonitor::CheckFile()//chage file data
 {
     Info newFileState(prevFileState.name);
     Change(newFileState.existence, newFileState.size);
     prevFileState = newFileState;
 }
 
-void FileMonitor::Change(bool existence, long size)
+void FileMonitor::Change(bool existence, long size)//change data
 {
     Notify(existence, size);
 }
