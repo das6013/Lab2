5222308bca963d898b509b6abe4034b1d4a68caf
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 12:32:17 +0700

    Update observe.h

--- observe.h
+++ observe.h
@@ -2,18 +2,18 @@
 #define OBSERVE_H
 
 
-class Observe
+class Observe//interface observe
 {
 public:
-    virtual void Update(bool existence, long size) = 0;
+    virtual void Update(bool existence, long size) = 0;//update data file bool exstine -check file,and size - size file
 };
 
-class FileObserver: Observe
+class FileObserver: Observe//concrete realize observe
 {
 private:
     long lastSize = -1;
 public:
-    void Update(bool existence, long size);
+    void Update(bool existence, long size);//update data file bool exstine -check file,and size - size file
 };
 
 #endif // OBSERVE_H
