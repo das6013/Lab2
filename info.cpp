9a1220fabace41e380f80260ae5f255762dbfac4
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 12:12:05 +0700

    add comment

--- info.cpp
+++ info.cpp
@@ -1,9 +1,9 @@
 #include "info.h"
 
-Info::Info(QString fileName)
+Info::Info(QString fileName)// constructor 
 {
-    name = fileName;
-    struct stat stat_buf;
-    existence = stat(fileName.toStdString().c_str(), &stat_buf) == 0;
-    size = stat_buf.st_size;
+    name = fileName;// file name
+    struct stat stat_buf;// stat_buf include dae file
+    existence = stat(fileName.toStdString().c_str(), &stat_buf) == 0;//check existence
+    size = stat_buf.st_size;//get size
 }
