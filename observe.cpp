3354bc27e0ecda65bb3deab9f0c77bc8826b0b42
Das3013 <46927879+das6013@users.noreply.github.com>
2022-06-30 12:37:23 +0700

    Update observe.cpp

--- observe.cpp
+++ observe.cpp
@@ -1,23 +1,23 @@
 #include "observe.h"
 #include<iostream>
 using namespace  std;
-void FileObserver::Update(bool existence, long size)
+void FileObserver::Update(bool existence, long size)//update data
 {
-    if (lastSize == -1) lastSize = size;
-    if (existence)
+    if (lastSize == -1) lastSize = size; //set size
+    if (existence)//check existence
     {
         if (lastSize != size)
         {
-            cout << "File is existung, file has been changed, size: " << size << endl;
+            cout << "File is existung, file has been changed, size: " << size << endl;//out
             lastSize = size;
         }
         else
         {
-            cout << "File is existing, size: " << lastSize <<endl;
+            cout << "File is existing, size: " << lastSize <<endl;//out
         }
     }
     else
     {
-        cout << "File is not existing" << endl;
+        cout << "File is not existing" << endl;//Error
     }
 }
